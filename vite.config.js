fd1057fac4f2f24160d7eec3655b9df0e8f5bef9
Doobystone <zdevbro@gmail.com>
2025-10-16 01:32:04 +0900

    Fix: Vercel deployment configruation

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    host: true
   }
-});
+})
