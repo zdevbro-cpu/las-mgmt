719cad191d6c92d2a15f41a0a01e1f49edb4810b
Kevin Kwak <zdevbro@gmail.com>
2025-11-04 12:48:15 +0900

    v2.0 근무일정관리

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'; // <--- 이 줄이 빠져있습니다!
 
 export default defineConfig({
   plugins: [react()],
